2b1099b245542834d1b51a64603a0a2c0a46de77
shubhabanerjee <shubhabanerjeewin8@gmail.com>
2020-11-23 13:39:27 +0530

    login page setup done

--- lib/Screens/LoginPage.dart
+++ lib/Screens/LoginPage.dart
@@ -4,8 +4,24 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child:Text("Center")),
-      
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/login.png'), fit: BoxFit.cover)),
+        child: Padding(
+          padding: EdgeInsets.only(
+              top: MediaQuery.of(context).size.height * 0.80,
+              left: MediaQuery.of(context).size.width * 0.20,
+              right: MediaQuery.of(context).size.width * 0.20),
+          child: InkWell(
+            onTap: () {
+              // Navigator.push(
+              //     context, MaterialPageRoute(builder: (_) => LoginPage()));
+            },
+            //child: Image.asset('assets/getstartedbtn.png'),
+          ),
+        ),
+      ),
     );
   }
-} 
+}
