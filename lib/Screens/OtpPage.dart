c7ef084850596d7d427b8548e37971ed1de8a50c
shubhabanerjee <shubhabanerjeewin8@gmail.com>
2020-12-12 12:03:41 +0530

    otp page done

--- lib/Screens/OtpPage.dart
+++ lib/Screens/OtpPage.dart
@@ -1,13 +1,68 @@
+import 'dart:convert';
+
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
+import 'package:fresh_on_the_go/Custome_Widget/const.dart';
 import 'package:fresh_on_the_go/Screens/LoginPage.dart';
 import 'package:velocity_x/velocity_x.dart';
+import 'package:http/http.dart' as http;
 
-class OtpPage extends StatelessWidget {
+class OtpPage extends StatefulWidget {
+  final String text;
+  OtpPage(this.text);
+
+  @override
+  _OtpPageState createState() => _OtpPageState();
+}
+
+class _OtpPageState extends State<OtpPage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  final _otpEditingController = TextEditingController();
+  verifyOtpPostRequest() async {
+    var network = await Connectivity().checkConnectivity();
+    print(network.index);
+    if (network.index == 2) {
+      _scaffoldKey.currentState.showSnackBar(SnackBar(
+        backgroundColor: kPrimaryColor,
+        content: Text('Please Check Your Internet Connection'),
+      ));
+    } else {
+      String url = "http://my-demo.xyz/farmers/apis/customer/upd_usr_after_otp";
+      final headers = {'Content-Type': 'application/json'};
+      Map<String, dynamic> body = {
+        "user_type": "3",
+        "email": widget.text.toString(),
+        "otp": _otpEditingController.text.toString()
+      };
+      String jsonBody = json.encode(body);
+      final response = await http.post(url, body: jsonBody, headers: headers);
+      var data = jsonDecode(response.body);
+      // var data = {"status": true, "message": "User created successfully."};
+      print(data);
+      if (data['status']) {
+        _scaffoldKey.currentState.showSnackBar(SnackBar(
+          backgroundColor: kPrimaryColor,
+          content: Text(data['message']),
+          duration: Duration(seconds: 1),
+        ));
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (_) => LoginPage()));
+      } else {
+        _scaffoldKey.currentState.showSnackBar(SnackBar(
+          backgroundColor: kPrimaryColor,
+          content: Text(data['message']),
+           duration: Duration(seconds: 1)
+        ));
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       body: SingleChildScrollView(
-        physics: NeverScrollableScrollPhysics(),
+        // physics: NeverScrollableScrollPhysics(),
         child: Container(
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
@@ -24,6 +79,7 @@ class OtpPage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 TextFormField(
+                  controller: _otpEditingController,
                   decoration: InputDecoration(
                     filled: true,
                     suffixIcon: Image.asset(
@@ -62,8 +118,7 @@ class OtpPage extends StatelessWidget {
                   width: MediaQuery.of(context).size.width * 0.41,
                   height: MediaQuery.of(context).size.height * 0.06,
                   child: InkWell(
-                    onTap: () => Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder: (_) => LoginPage())),
+                    onTap: () => verifyOtpPostRequest(),
                     child: Image.asset(
                       'assets/getstartedbtn.png',
                       fit: BoxFit.cover,
