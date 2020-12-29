dbfbc0ef5238031425ff76b37fb998d22cab587c
shubhabanerjee <shubhabanerjeewin8@gmail.com>
2020-12-29 22:07:04 +0530

    initial commit

--- lib/Screens/ForgetPasswordPage.dart
+++ lib/Screens/ForgetPasswordPage.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:FreshOnTheGo/Custome_Widget/const.dart';
 import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
@@ -62,74 +61,72 @@ class _ForgetPasswordPageState extends State<ForgetPasswordPage> {
     return Scaffold(
       key: _scaffoldKey,
       body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/login.png'), fit: BoxFit.cover)),
-          child: Padding(
-            padding: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height * 0.65,
-                left: MediaQuery.of(context).size.width * 0.10,
-                right: MediaQuery.of(context).size.width * 0.10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                TextFormField(
-                  controller: _forgetEditingController,
-                  decoration: InputDecoration(
-                    filled: true,
+          physics: BouncingScrollPhysics(),
+          child: Container(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/login.png'),
+                      fit: BoxFit.cover)),
+              child: Padding(
+                  padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.65,
+                      left: MediaQuery.of(context).size.width * 0.10,
+                      right: MediaQuery.of(context).size.width * 0.10),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        TextFormField(
+                          controller: _forgetEditingController,
+                          decoration: InputDecoration(
+                            filled: true,
 
-                    suffixIcon: Image.asset(
-                      'usernameico.png',
-                      width: 10,
-                    ).pOnly(top: 10, bottom: 10, right: 10),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                      borderSide: BorderSide(
-                        color: Colors.transparent,
-                      ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                      borderSide: BorderSide(
-                        color: Colors.transparent,
-                        // width: 2.0,
-                      ),
-                    ),
-                    contentPadding: const EdgeInsets.all(8.0),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(100.0),
-                    ),
-                    fillColor: Color(0xFFFFD552), //fromRGBO(255, 213, 82,0.0),
-                    hintText: "Email usuário",
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.41,
-                  height: MediaQuery.of(context).size.height * 0.06,
-                  child: InkWell(
-                    onTap: () => postEmail(),
-                    child: Image.asset(
-                      'assets/getstartedbtn.png',
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+                            suffixIcon: Image.asset(
+                              'usernameico.png',
+                              width: 10,
+                            ).pOnly(top: 10, bottom: 10, right: 10),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                              borderSide: BorderSide(
+                                color: Colors.transparent,
+                              ),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25.0),
+                              borderSide: BorderSide(
+                                color: Colors.transparent,
+                                // width: 2.0,
+                              ),
+                            ),
+                            contentPadding: const EdgeInsets.all(8.0),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(100.0),
+                            ),
+                            fillColor:
+                                Color(0xFFFFD552), //fromRGBO(255, 213, 82,0.0),
+                            hintText: "Email usuário",
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.41,
+                          height: MediaQuery.of(context).size.height * 0.06,
+                          child: InkWell(
+                            onTap: () => postEmail(),
+                            child: Image.asset(
+                              'assets/getstartedbtn.png',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        )
+                      ])))),
     );
   }
 }
